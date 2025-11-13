b794eed3ee50a4618b9764948b5a95a2ca87f0ed
Mansi_Patel <patelmaya9812@gmail.com>
2025-11-13 20:43:41 +0530

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -56,7 +56,7 @@ const projects = [
     title: "Easify",
     description:
       "A modern, minimal, and interactive journal website featuring collapsible cards with tips and exercises for productivity, business, health, lifestyle, and work, plus fun facts, smooth animations, and responsive gradient navigation.",
-    link: "https://mansicardportfolio.vercel.app/",
+    link: "https://easify-one.vercel.app/",
     category: "Frontend",
     icon: <FileCode className="w-4 h-4" />,
   },
